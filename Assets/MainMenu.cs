529a783f31861278d9658af3ebbceade365d4d11
Cade Disselkoen <zosimus.this@gmail.com>
2021-01-17 17:49:20 -0800

    YEYE

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button newGameButton;
+    public Button exitButton; 
+    public string newGameSceneName;
+
+    public void Awake() {
+        newGameButton.onClick.AddListener(NewGame);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+    public void NewGame() {
+        SceneManager.LoadScene(newGameSceneName);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
